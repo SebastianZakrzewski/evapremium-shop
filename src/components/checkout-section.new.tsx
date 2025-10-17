bc7ca855bc590d43573264e4b356d3b70f69396f
SebastianZakrzewski <devzakrzewski@gmail.com>
2025-10-17 03:01:53 +0200

    feat: zmień tekst przycisku z 'Złóż zamówienie' na 'Zapłać teraz'
    - Zaktualizowano tekst przycisku finalizacji zamówienia
    - Bardziej akcyjny tekst zachęca do sfinalizowania płatności
    - Poprawia UX i konwersję w procesie checkout

--- src/components/checkout-section.new.tsx
+++ src/components/checkout-section.new.tsx
@@ -926,7 +926,7 @@ export default function CheckoutSectionNew() {
                     ) : (
                       <>
                         <Check className="w-5 h-5 mr-3" />
-                        Złóż zamówienie
+                        Zapłać teraz
                       </>
                     )}
                   </Button>
