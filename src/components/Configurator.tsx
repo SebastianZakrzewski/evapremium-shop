e1b0dd24c111a3b14a127b957a373762cfedf10f
SebastianZakrzewski <devzakrzewski@gmail.com>
2025-11-08 05:49:26 +0100

    style: zmniejszenie rozmiaru sekcji konfiguratora na duÅ¼ych ekranach do 80%
    - Zmniejszono max-width kontenera z max-w-[1152px] do max-w-[1024px] (80% z oryginalnych 1280px)
    - Zmniejszono max-width dla 2xl z 1440px do 1280px (80% z oryginalnych 1600px)
    - Zmniejszono szerokoÅ›Ä‡ lewej kolumny (wizualizacja): lg:w-[720px] xl:w-[800px] 2xl:w-[880px] (80% z oryginalnych wartoÅ›ci)
    - Zmniejszono szerokoÅ›Ä‡ prawej kolumny (konfigurator): lg:w-[560px] xl:w-[624px] 2xl:w-[720px] (80% z oryginalnych wartoÅ›ci)
    - Zmniejszono gap miÄ™dzy kolumnami: lg:gap-8 2xl:gap-[38.4px] (80% z oryginalnych wartoÅ›ci)

--- src/components/Configurator.tsx
+++ src/components/Configurator.tsx
@@ -929,10 +929,10 @@ export default function Configurator() {
 
   return (
     <section className="w-full bg-black text-white">
-      <div className="max-w-[1152px] 2xl:max-w-[1440px] mx-auto px-4 sm:px-8 py-6 md:py-12 lg:py-16">
-        <div className="flex flex-col md:flex-row gap-6 lg:gap-9 2xl:gap-11">
+      <div className="max-w-[1024px] 2xl:max-w-[1280px] mx-auto px-4 sm:px-8 py-6 md:py-12 lg:py-16">
+        <div className="flex flex-col md:flex-row gap-6 lg:gap-8 2xl:gap-[38.4px]">
           {/* Lewa strona - wizualizacja */}
-          <div className="w-full lg:w-[810px] xl:w-[900px] 2xl:w-[990px]">
+          <div className="w-full lg:w-[720px] xl:w-[800px] 2xl:w-[880px]">
             <div 
               ref={previewRef}
               className="relative w-full h-[40vh] md:h-[550px] lg:h-[650px] xl:h-[700px] 2xl:h-[800px] rounded-xl overflow-hidden border border-neutral-800 bg-neutral-950 cursor-pointer md:cursor-default transition-opacity duration-300 sticky top-16 md:sticky md:top-20 z-30 border-b md:border-b-0"
@@ -1007,7 +1007,7 @@ export default function Configurator() {
           {/* Prawa strona - konfigurator z sekcjami */}
           <div 
             ref={configPanelRef}
-            className="w-full lg:w-[630px] xl:w-[702px] 2xl:w-[810px] bg-neutral-950/60 border border-neutral-800 rounded-2xl p-6 md:p-8 lg:p-10 2xl:p-12 min-h-[400px] sm:min-h-[500px] md:h-auto flex flex-col pb-32 md:pb-24 h-[calc(60vh-80px)] md:h-auto max-h-[calc(60vh-80px)] md:max-h-none overflow-y-auto md:overflow-visible overflow-x-hidden"
+            className="w-full lg:w-[560px] xl:w-[624px] 2xl:w-[720px] bg-neutral-950/60 border border-neutral-800 rounded-2xl p-6 md:p-8 lg:p-10 2xl:p-12 min-h-[400px] sm:min-h-[500px] md:h-auto flex flex-col pb-32 md:pb-24 h-[calc(60vh-80px)] md:h-auto max-h-[calc(60vh-80px)] md:max-h-none overflow-y-auto md:overflow-visible overflow-x-hidden"
             data-config-panel
             onTouchStart={onTouchStart}
             onTouchMove={onTouchMove}
